300d81dee20ae198b9ce7d7b8ba28f3111c47748
yujin <glisteneugene@gmail.com>
2021-02-15 19:57:27 +0900

    day 18

---
+++ BOJ/DAY_18.py
@@ -0,0 +1,29 @@
+# 210212
+# 11653
+def isPrime(num):
+    if num == 1:
+        return False
+    for i in range(2, int(num**0.5)+1):
+        if not num % i:
+            return False
+    return True
+
+
+n = int(input())
+c = n
+res = []
+while True:
+    if n == 1:
+        break
+    if isPrime(n):
+        res.append(n)
+        break
+    for i in range(2, c):
+        if isPrime(i):
+            if n % i == 0:
+                res.append(i)
+                n = n // i
+            else:
+                continue
+res.sort()
+print('\n'.join(map(str, res)))
