cfe52bc0b4cab92abf18fa51be7abc1b278bed37
yujin <glisteneugene@gmail.com>
2021-03-06 15:01:53 +0900

    뒤집은 소수 다른방법

--- Inflearn/Section2/reverse_prime.py
+++ Inflearn/Section2/reverse_prime.py
@@ -6,20 +6,31 @@ num = list(map(int, input().split()))
 
 
 def reverse(x):
+    '''
+    문자열 사용
     res = ''
     while x > 0:
         res += str(x % 10)
         x = x // 10
     return int(res)
+    '''
+    # 숫자 사용
+    res = 0
+    while x > 0:
+        t = x % 10
+        res = res*10 + t
+        x = x // 10
+    return res
 
 
 def isPrime(x):
     if x == 1:
         return False
-    for i in range(2, int(x**0.5)+1):
+    for i in range(2, x//2+1):
         if not (x % i):
             return False
-    return True
+    else:
+        return True
 
 
 for x in num:
