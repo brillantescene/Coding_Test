2d11b60240bac4f1f14c33086928786acc5fa5af
yujin <glisteneugene@gmail.com>
2021-05-08 12:06:00 +0900

    cut edge tech

--- Inflearn/Section6/puppy.py
+++ Inflearn/Section6/puppy.py
@@ -1,17 +1,19 @@
 import sys
-sys.stdin = open('Inflearn/in1.txt', 'r')
+sys.stdin = open('Inflearn/in2.txt', 'r')
 
 
-def DFS(idx, sum):
+def DFS(idx, sum, tsum):
     global res
+    if sum + (total-tsum) < res:
+        return
     if sum > c:
         return
     if idx == n:
         if res < sum:
             res = sum
     else:
-        DFS(idx+1, sum+p[idx])
-        DFS(idx+1, sum)
+        DFS(idx+1, sum+p[idx], tsum+p[idx])
+        DFS(idx+1, sum, tsum+p[idx])
 
 
 if __name__ == "__main__":
@@ -20,7 +22,7 @@ if __name__ == "__main__":
 
     for i in range(n):
         p[i] = int(input())
-
+    total = sum(p)
     res = 0
-    DFS(0, 0)
+    DFS(0, 0, 0)
     print(res)
