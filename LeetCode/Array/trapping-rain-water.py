c8049e8d0e3d0f8b06d9259d4da77045c14488fb
yujin <glisteneugene@gmail.com>
2021-06-08 19:54:12 +0900

    trapping rain water

---
+++ LeetCode/Array/trapping-rain-water.py
@@ -0,0 +1,23 @@
+class Solution:
+    # 투 포인터 사용
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+
+        volume = 0
+        left, right = 0, len(height)-1
+        left_max, right_max = height[left], height[right]
+
+        while left < right:
+            left_max, right_max = max(height[left], left_max), max(
+                height[right], right_max)
+
+            # 더 높은 쪽으로 투 포인터 이동
+            if left_max <= right_max:
+                volume += left_max - height[left]
+                left += 1
+            else:
+                volume += right_max - height[right]
+                right -= 1
+
+        return volume
