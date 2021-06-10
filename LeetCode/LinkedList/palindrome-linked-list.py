584d43f4c03ec09c022e08cce6a2d05fb141ed89
yujin <glisteneugene@gmail.com>
2021-06-10 15:06:06 +0900

    palindrome linked list -> runner

--- LeetCode/LinkedList/palindrome-linked-list.py
+++ LeetCode/LinkedList/palindrome-linked-list.py
@@ -3,6 +3,26 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# 런너를 이용한 풀이
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        rev = None
+        slow = fast = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
+
+        if fast:
+            slow = slow.next
+
+        while rev and rev.val == slow.val:
+            slow, rev = slow.next, rev.next
+
+        return not rev
+
+
+'''
 class Solution:
     def isPalindrome(self, head: ListNode) -> bool:
 
@@ -23,3 +43,4 @@ class Solution:
                 return False
 
         return True
+'''
