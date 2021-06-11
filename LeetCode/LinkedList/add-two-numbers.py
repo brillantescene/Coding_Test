9e880ab9a246a70db16dbfcf12b7a28e346d2ee7
yujin <glisteneugene@gmail.com>
2021-06-11 13:00:33 +0900

    functool lambda 사용

--- LeetCode/LinkedList/add-two-numbers.py
+++ LeetCode/LinkedList/add-two-numbers.py
@@ -33,7 +33,6 @@ class Solution:
         a = self.toList(self.reverseList(l1))
         b = self.toList(self.reverseList(l2))
 
-        resultStr = int(''.join(str(e) for e in a)) + \
-            int(''.join(str(e) for e in b))
-
+        resultStr = functools.reduce(
+            lambda x, y: 10*x+y, a) + functools.reduce(lambda x, y: 10*x+y, b)
         return self.toReversedLinkedList(str(resultStr))
