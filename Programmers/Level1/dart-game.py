2a4074676883cd47ec7a26520ffd5598c267763d
yujin <glisteneugene@gmail.com>
2021-06-13 13:30:42 +0900

    2018 카카오 공채 1차 다트 게임 - 수정

--- Programmers/Level1/dart-game.py
+++ Programmers/Level1/dart-game.py
@@ -2,24 +2,26 @@
 # 다트게임
 
 def solution(dartResult):
-    answer = 0
     result = []
     bonus = {'S': 1, 'D': 2, 'T': 3}
-    for i in range(len(dartResult)):
-        if dartResult[i].isdigit():
-            if result and dartResult[i-1].isdigit():
-                result[-1] = result[-1]*10+int(dartResult[i])
+    tmp = ''
+    for d in dartResult:
+        if d.isdigit():
+            if result and tmp.isdigit():
+                result[-1] = result[-1]*10+int(d)
             else:
-                result.append(int(dartResult[i]))
-        if dartResult[i] in ['S', 'D', 'T']:
-            result[-1] = result[-1] ** bonus[dartResult[i]]
-        if dartResult[i] == '*':
+                result.append(int(d))
+                tmp = d
+
+        if d in ['S', 'D', 'T']:
+            result[-1] = result[-1] ** bonus[d]
+            tmp = ''
+        if d == '*':
             result[-1] = result[-1] * 2
             if len(result) > 1:
                 result[-2] = result[-2] * 2
-        if dartResult[i] == '#':
+        if d == '#':
             result[-1] = - result[-1]
-    print(result)
     return sum(result)
 
 
