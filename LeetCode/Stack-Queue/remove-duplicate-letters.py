bbb51d6a35afb7d2fd6c2611ac57caa5a6ca579c
yujin <glisteneugene@gmail.com>
2021-08-19 13:40:46 +0900

    중복 문자 제거

---
+++ LeetCode/Stack-Queue/remove-duplicate-letters.py
@@ -0,0 +1,25 @@
+# 재귀
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        for char in sorted(set(s)):
+            suffix = s[s.index(char):]
+            if set(s) == set(suffix):
+                return char + self.removeDuplicateLetters(suffix.replace(char, ''))
+        return ''
+
+# 스택
+
+
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        counter, seen, stack = collections.Counter(s), set(), []
+
+        for char in s:
+            counter[char] -= 1
+            if char in seen:
+                continue
+            while stack and char < stack[-1] and counter[stack[-1]] > 0:
+                seen.remove(stack.pop())
+            stack.append(char)
+            seen.add(char)
+        return ''.join(stack)
