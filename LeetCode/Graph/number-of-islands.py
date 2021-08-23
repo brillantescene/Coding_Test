3e6efadd0c35656b6334ffffd9339d1bf704cdfb
yujin <glisteneugene@gmail.com>
2021-08-23 21:04:55 +0900

    섬의 개수

---
+++ LeetCode/Graph/number-of-islands.py
@@ -0,0 +1,50 @@
+'''
+class Solution:
+    def dfs(self, grid: List[List[str]], i: int, j: int):
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != '1':
+            return
+        grid[i][j] = '0'
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i, j+1)
+        self.dfs(grid, i, j-1)
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+        count = 0
+        
+        if not grid:
+            return 0
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
+                    count += 1
+        return count
+'''
+
+# 중첩 함수를 쓰면 이렇게 편해진다는 것!!!
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(i, j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != '1':
+                return
+            grid[i][j] = '0'
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
+
+        count = 0
+
+        if not grid:
+            return 0
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    dfs(i, j)
+                    count += 1
+        return count
