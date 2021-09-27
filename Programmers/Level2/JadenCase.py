29481e0530957848369d70f648fa47ecbdc4b8ba
yujin <glisteneugene@gmail.com>
2021-09-27 20:28:24 +0900

    JadenCase 문자열 만들기

---
+++ Programmers/Level2/JadenCase.py
@@ -0,0 +1,9 @@
+def solution(s):
+    # s = s.lower()
+    words = s.lower().split(' ')
+    for i in range(len(words)):
+        words[i] = words[i].capitalize()
+    return ' '.join(words)
+
+
+print(solution("3people unFollowed me"))
